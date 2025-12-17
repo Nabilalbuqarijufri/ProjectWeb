794e5dbd020c1aa8115ff6a65affa4fc44dd1950
dafwa <daffafahir@gmail.com>
2025-12-17 11:56:28 +0800

    added register, login and logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,31 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Register;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('login', function () {
-    return view('login', ['title' => 'Login Page']);
-});
+// Register Routes
+Route::view('/register', 'register', ['title' => 'Register Page'])
+    ->middleware('guest')
+    ->name('register');
 
-Route::get('register', function () {
-    return view('register', ['title' => 'Register Page']);
-});
+Route::post('/register', Register::class)
+    ->middleware('guest');
+
+// Login Route
+Route::view('/login', 'login', ['title' => 'Login Page'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+// Logout Route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
